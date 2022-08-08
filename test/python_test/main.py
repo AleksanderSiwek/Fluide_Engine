7b9774262ee7f3e14f51f6a0442d835211c2c9f8
AleksanderSiwek <aleksander@tuinel.eu>
2022-08-08 20:20:04 +0200

    Base Wrapper

--- test/python_test/main.py
+++ test/python_test/main.py
@@ -6,30 +6,48 @@ os.add_dll_directory(os.path.join(os.environ['CUDA_PATH'], 'bin'))
 
 import PyFluidEngine as pyf
 
+def RunTestSimulation(simulator, numberOfIterations, timeIntervalInSeconds, cacheDir, outFilePrefix, size, domain):
+    # Load Initial state
+    fluidMesh = pyf.TriangleMesh()
+    objLoader = pyf.OBJManager()
+    objLoader.Load("../test_cases/short_water_wall.obj", fluidMesh)
+
+    # Setup colliders
+    colliderMesh_1 = pyf.TriangleMesh()
+    colliderMesh_2 = pyf.TriangleMesh()
+    objLoader.Load("../test_cases/collider_2.obj", colliderMesh_1)
+    objLoader.Load("../test_cases/test_cube.obj", colliderMesh_2)
+
+    # Setup Simulator
+    simulator.AddExternalForce(pyf.DirectionalField(pyf.Vector3D(0.0, -9.8, 0.0)))
+    simulator.SetViscosity(0)
+    simulator.SetMaxClf(3)
+    simulator.InitializeFromTriangleMesh(fluidMesh)
+
+    # Run simulation
+    runner = pyf.SimulationRunner()
+    runner.RunSimulation(simulator, timeIntervalInSeconds, numberOfIterations, outFilePrefix, cacheDir)
+    print("Simulation ended!")
+    runner.GetStats().PrintStats()
+    print("\n")
+
+
 timeIntervalInSeconds = 1.0 / 30.0
+numberOfIterations = 5
 
 frame = pyf.Frame(timeIntervalInSeconds)
-domain = pyf.BoundingBox3D(pyf.Vector3D(0.0, 0.0, 0.0), pyf.Vector3D(4.0, 8.0, 8.0))
+domain = pyf.BoundingBox3D(pyf.Vector3D(0.0, 0.0, 0.0), pyf.Vector3D(4.0, 4.0, 4.0))
 mesh = pyf.TriangleMesh()
 gravity = pyf.DirectionalField(pyf.Vector3D(0.0, -9.8, 0.0))
 objLoader = pyf.OBJManager()
 
 objLoader.Load("../test_cases/short_water_wall.obj", mesh)
+size = pyf.Vector3S(20, 20, 20)
 
-simulator = pyf.PICSimulator(pyf.Vector3S(20, 40, 40), domain);
-simulator.AddExternalForce(gravity)
-simulator.SetViscosity(0)
-simulator.SetMaxClf(3)
-simulator.SetMaxClf(3)
-simulator.SetCurrentFrame(frame)
-simulator.InitializeFromTriangleMesh(mesh)
+picSimulator = pyf.PICSimulator(size, domain);
+apicSimulator = pyf.APICSimulator(size, domain);
+flipSimulator = pyf.FLIPSimulator(size, domain);
 
-for i in range(25):
-    print("Iteration = ", i)
-    simulator.AdvanceSingleFrame()
-    simulator.GetSurface(mesh)
-    objLoader.Save("../../" + "python_pic_test" + "_" + str(i) + ".obj", mesh)
-    mesh.Clear();
-
-print(simulator)
-# print(pyf.PICSimulator(pyf.Vector3(1, 1, 1), domain))
+RunTestSimulation(picSimulator, numberOfIterations, timeIntervalInSeconds, "../../simOutputs/pic_py_test", "pic", size, domain)
+RunTestSimulation(apicSimulator, numberOfIterations, timeIntervalInSeconds, "../../simOutputs/apic_py_test", "apic", size, domain)
+RunTestSimulation(flipSimulator, numberOfIterations, timeIntervalInSeconds, "../../simOutputs/flip_py_test", "flip", size, domain)
